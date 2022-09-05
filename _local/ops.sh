e465ec2323ab2101155e3745bb5779125d68c700
mirage335 <spamfreemirage335@gmail.com>
2022-09-05 14:27:17 -0500

    Default to distro freecad if setups directory does not exist.

--- _local/ops.sh
+++ _local/ops.sh
@@ -6,3 +6,11 @@ export ub_anchor_autoupgrade="true"
 #export ub_anchor_specificSoftwareName="experimental"
 export ub_anchor_user="true"
 
+
+if ! [[ -d "$scriptLocal" ]]
+then
+	_app_command() {
+		_app_command-distro "$@"
+	}
+fi
+
